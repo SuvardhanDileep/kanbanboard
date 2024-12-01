00da014be69d52bca5afea64d67fa66a7bd5c2c7
SuvardhanDileep <gsuvardhandileep@gmail.com>
2024-12-01 11:31:32 +0530

    deploy final

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Card.css";
 import done from "../../assets/Done.svg"
+import todo from "../../assets/To-do.svg"
+import backlogicon from "../../assets/Backlog.svg";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import inprogress from "../../assets/in-progress.svg";
 import {BsFillExclamationSquareFill } from "react-icons/bs";
@@ -24,7 +26,7 @@ const Card = ({ id, title, tag, status, priority }) => {
           <img
             style={{ width: "95%", height: "95%", borderRadius: "50%" }}
             src="https://i.postimg.cc/qvY7N7W0/png-clipart-computer-icons-user-profile-info-miscellaneous-face.png"
-            alt="UserImage"
+            alt=""
           />
           <div className="showStatus"></div>
         </div>
@@ -33,23 +35,16 @@ const Card = ({ id, title, tag, status, priority }) => {
         {!isStatus &&
           (
             status === "Backlog" ? (
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" stroke="#95999F" stroke-width="2" stroke-dasharray="1.4 1.74"/>
-</svg>
+              <img src={backlogicon} alt="baclogicon"/>
 
             )
               : status === "Todo" ? (
-                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" stroke="#B8B8B8" stroke-width="2"/>
-</svg>
+                <img src={todo} alt="To-Do"/>
               ) : status === "In progress" ? (
-                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="white" stroke="#F2BE00" stroke-width="2"/>
-<path d="M9 7C9 5.89543 8.10457 5 7 5C5.89543 5 5 5.89543 5 7C5 8.10457 5.89543 9 7 9C8.10457 9 9 8.10457 9 7Z" stroke="#F2BE00" stroke-width="4"/>
-</svg>
+                <img src={inprogress} alt="In progress" />
 
               ) : status === "Done" ? (
-                <img src={done} alt="Done"></img>
+                <img src={done} alt="Done"/>
               )
                 : (
                   <IoMdCloseCircleOutline />
